d984c4f4ed3764c8429cfbf9002335da6d16d071
Heather Park <hthr.prk@gmail.com>
2016-06-28 17:40:34 -0700

    Have POST response send back messages in correct order

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,4 @@
+
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -5,50 +6,55 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 // Seconds.
 };
 
-var data = '';
+var headers = defaultCorsHeaders;
+headers['Content-Type'] = "application/json";
+
+var sendResponse = function(res, data, statusCode) {
+  statusCode = statusCode || 200;
+  res.writeHead(statusCode, headers);
+  res.end(JSON.stringify(data));
+};
+
+var messages = [];
+var messageId = 1;
+
+var methods = {
+  GET: function(req, res) {
+    sendResponse(res, {results: messages});
+  },
+  POST: function(req, res) {
+    collectData(req, function(data) {
+      data.messageId = messageId++;
+      messages.push(data);
+      sendResponse(res, {messageId: data.messageId}, 201);
+    });
+  },
+  OPTIONS: function(req, res) {
+    sendResponse(res, null);
+  }
+};
+
+var collectData = function(req, cb) {
+  var data = '';
+  req.on('data', function(chunk) {
+    data += chunk;
+  });
+  req.on('end', function() {
+    cb(JSON.parse(data));
+  });
+};
+
+
 
 
 module.exports = function(request, response) {
   //console.log("Serving request type " + request.method + " for url " + request.url);
-
-  var statusCode = 200;
-
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "application/json";
-
-  var sendResponse = function(res, data, statusCode) {
-    statusCode = statusCode || 200;
-    res.writeHead(statusCode, headers);
-    res.end(JSON.stringify(data));
-  };
-
-  var collectData = function(req, cb) {
-    var data = '';
-    req.on('data', function(chunk) {
-      data += chunk;
-    });
-
-    req.on('end', function() {
-      cb(JSON.parse(data));
-    });
-  };
-
-  var methods = {
-    GET: function(req, res) {
-      var data = {result: 'GET request made'};
-      sendResponse(data, headers);
-    },
-    POST: function(req, res) {
-      collectData(req, function() {
-        var successData = {result: 'POST request made'};
-        sendResponse(successData, headers, 201);
-      });
-    },
-    OPTIONS: function(req, res) {
-      sendResponse(res, null);
-    }
-  };
+  if (methods[request.method]) {
+    var method = methods[request.method];
+    method(request, response);
+  } else {
+    sendResponse(response, {error: 'NOT FOUND'}, 404);
+  }
 };
 
 
